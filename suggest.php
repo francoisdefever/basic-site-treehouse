e27b851d0898e488cf296ad43394e7ae325519d6
francoisdefever <fdefever.work@gmail.com>
2016-04-01 09:59:51 +0200

    Test debugging php on catalog.php | _GET contents

--- suggest.php
+++ suggest.php
@@ -2,9 +2,9 @@
 
 $pageTitle = 'Suggest a Media Item';
 
-include("inc/header.php"); ?>
+include('inc/header.php'); ?>
 
-<div class="section page">
+<div class='section page'>
 	<h1>Suggest a Media Item</h1>
 </div>
-<?php include("inc/footer.php"); ?>
+<?php include('inc/footer.php'); ?>
